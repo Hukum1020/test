9f1c46959efb248809407151737c278c078218c3
Hukum1020 <nikitasicily22@gmail.com>
2025-03-26 11:25:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def messages():
     return jsonify(get_messages())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Railway даёт свой порт
+    app.run(host='0.0.0.0', port=port)
